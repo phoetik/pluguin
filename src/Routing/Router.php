ceb6ce809edc7c13e25088823c3060fb4143ba20
StingyFickle <informmenewshmm@gmail.com>
2022-06-30 18:06:06 +0430

    .

---
+++ src/Routing/Router.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Pluguin\Routing;
+
+class Router
+{
+    public function __construct(Dispatcher $events, Container $container = null)
+    {
+        $this->events = $events;
+        $this->routes = new RouteCollection;
+        $this->container = $container ?: new Container;
+    }
+
+    /**
+     * Register a new GET route with the router.
+     *
+     * @param  string  $uri
+     * @param  array|string|callable|null  $action
+     * @return \Illuminate\Routing\Route
+     */
+    public function get($uri, $action = null)
+    {
+        return $this->addRoute(['GET', 'HEAD'], $uri, $action);
+    }
+
+    /**
+     * Register a new POST route with the router.
+     *
+     * @param  string  $uri
+     * @param  array|string|callable|null  $action
+     * @return \Illuminate\Routing\Route
+     */
+    public function post($uri, $action = null)
+    {
+        return $this->addRoute('POST', $uri, $action);
+    }
+
+    /**
+     * Register a new PUT route with the router.
+     *
+     * @param  string  $uri
+     * @param  array|string|callable|null  $action
+     * @return \Illuminate\Routing\Route
+     */
+    public function put($uri, $action = null)
+    {
+        return $this->addRoute('PUT', $uri, $action);
+    }
+
+    /**
+     * Register a new PATCH route with the router.
+     *
+     * @param  string  $uri
+     * @param  array|string|callable|null  $action
+     * @return \Illuminate\Routing\Route
+     */
+    public function patch($uri, $action = null)
+    {
+        return $this->addRoute('PATCH', $uri, $action);
+    }
+
+    /**
+     * Register a new DELETE route with the router.
+     *
+     * @param  string  $uri
+     * @param  array|string|callable|null  $action
+     * @return \Illuminate\Routing\Route
+     */
+    public function delete($uri, $action = null)
+    {
+        return $this->addRoute('DELETE', $uri, $action);
+    }
+
+    /**
+     * Register a new OPTIONS route with the router.
+     *
+     * @param  string  $uri
+     * @param  array|string|callable|null  $action
+     * @return \Illuminate\Routing\Route
+     */
+    public function options($uri, $action = null)
+    {
+        return $this->addRoute('OPTIONS', $uri, $action);
+    }
+
+    /**
+     * Register a new route responding to all verbs.
+     *
+     * @param  string  $uri
+     * @param  array|string|callable|null  $action
+     * @return \Illuminate\Routing\Route
+     */
+    public function any($uri, $action = null)
+    {
+        return $this->addRoute(self::$verbs, $uri, $action);
+    }
+    
+    /**
+     * Register a new Fallback route with the router.
+     *
+     * @param  array|string|callable|null  $action
+     * @return \Illuminate\Routing\Route
+     */
+    public function fallback($action)
+    {
+        $placeholder = 'fallbackPlaceholder';
+
+        return $this->addRoute(
+            'GET', "{{$placeholder}}", $action
+        )->where($placeholder, '.*')->fallback();
+    }
+}
